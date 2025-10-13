2e308746b7a163a71a1c07743beaa8837d59ec71
Desmond <63413811+desmondcoles1@users.noreply.github.com>
2025-10-13 14:33:13 -0500

    updated notes.

--- rust_exercises/leetcde_345_reverse_vowels_in_string/src/main.rs
+++ rust_exercises/leetcde_345_reverse_vowels_in_string/src/main.rs
@@ -1,5 +1,7 @@
 pub fn reverse_vowels(s: String) -> String {
-    
+    let vowels = ['a','e','i','o','u','A','E','I','O','U'];
+    //I should go down the string, find the i values where there is a value, use a for loop, then store those values and reverse them,
+    //maybe use match to make a dictionary from i values to vowels?
 }
 
 ///this feels like such a slow solution!
