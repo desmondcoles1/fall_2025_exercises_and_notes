dc771e0f5e0aebae1871a09de1a4ecb930104a16
Desmond <63413811+desmondcoles1@users.noreply.github.com>
2025-10-13 10:51:09 -0500

    started leetcode 345

---
+++ rust_exercises/leetcde_345_reverse_vowels_in_string/src/main.rs
@@ -0,0 +1,14 @@
+pub fn reverse_vowels(s: String) -> String {
+    
+}
+
+///this feels like such a slow solution!
+
+
+//////testing
+
+
+fn main() {
+    let Strng= ".";
+    println!("{:?}", reverse_vowels(Strng);
+}
