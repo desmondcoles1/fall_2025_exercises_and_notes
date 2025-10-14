892b973cf8f51e7622ce4d70430e3cde28bc7f3d
Desmond <63413811+desmondcoles1@users.noreply.github.com>
2025-10-14 13:09:32 -0500

    solved leetcode 238

---
+++ rust_exercises/leetcde_238_product_of_array_except_self/src/main.rs
@@ -0,0 +1,41 @@
+pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+    //initiailizing variables
+    let mut total_product = 1; // product of everything in array
+    let mut zero_counter = 0; //how many zeros in input
+    let mut zero_position = 0; // where is the zero
+    let num_nums = nums.len(); //length of relevant vectors
+    let mut product_except_self_vector: Vec<i32> = vec![0; num_nums];  //result 
+    for i in 0..num_nums{
+        if nums[i]== 0{
+            zero_counter = zero_counter+1;
+            zero_position = i;
+            continue
+        }
+        if zero_counter == 2{
+            break
+        }
+        total_product = total_product * nums[i]
+    }
+    if zero_counter == 2{
+        //do nothing, leave it as 0s
+    }
+    if zero_counter == 1{
+        product_except_self_vector[zero_position] = total_product;
+    }
+    if  zero_counter == 0{
+        for i in 0..num_nums{
+            product_except_self_vector[i] = total_product / nums[i] as i32;
+        }
+    }
+    return product_except_self_vector
+}
+
+///test
+
+fn main() {
+    let nums = [0,1,2,3,4,5,11,0].to_vec();
+    println!("{:?}", product_except_self(nums));
+}
+
+
+//this solution was slow
