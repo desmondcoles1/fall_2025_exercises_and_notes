7e00923c700c8e6fcf27be5ebd90af988fa5eedd
Desmond <63413811+desmondcoles1@users.noreply.github.com>
2025-10-18 18:37:04 -0400

    found better solution to leetcode problem

--- rust_exercises/leetcde_283_move_zeros/src/main.rs
+++ rust_exercises/leetcde_283_move_zeros/src/main.rs
@@ -9,10 +9,8 @@ pub fn move_zeroes(nums: &mut Vec<i32>) {
                 nums[j]= nums[j+1]
             }
             nums[n-1] = 0;
-        }
-        println!("{:?}", nums)
+        } 
     }
-    println!("{:?}", nums)
 }
 
 ///test
@@ -21,6 +19,26 @@ fn main() {
     let mut nums = [0,0,0,1,0,0,0,0,2,0,1,1,1,0,2,20,1,0,0,0].to_vec();
     move_zeroes(&mut nums);
     //println!("{:?}", move_zeroes(&mut nums))
+}
+
+
+//my solution is not so good, this is better:
+
+pub fn move_zeroes_better(nums: &mut Vec<i32>) {
+    let mut insert_pos = 0;
+
+    // Move all non-zero elements forward
+    for i in 0..nums.len() {
+        if nums[i] != 0 {
+            nums[insert_pos] = nums[i];
+            insert_pos += 1;
+        }
+    }
+
+    // Fill the rest with zeros
+    for i in insert_pos..nums.len() {
+        nums[i] = 0;
+    }
 }
 
 
